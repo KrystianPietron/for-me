d19284955049242b3eda5d3898471da0df629b6f
Krystian Pietroń <krystianpietr@gmail.com>
2018-12-29 22:32:35 +0100

    initial commit and add element Button and clear files

---
+++ src/elements/Button.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton'
+
+const Button = props => (
+    <RaisedButton 
+    label={props.label}
+    onClick={props.onClick}
+    primary={props.primary}
+    style={props.style}
+    />
+)
+
+export default Button 
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <MuiThemeProvider>
+            <App />
+        </MuiThemeProvider>
+    </Provider>,
+    document.getElementById('root'));
+
